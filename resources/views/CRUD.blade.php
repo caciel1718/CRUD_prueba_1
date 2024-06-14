39f034503df09844b4ae8019d3c8760c0282ff36
Caciel <caciel.1998@gmail.com>
2024-06-14 14:41:11 -0600

    Se realizo cambias en el diseño y el formulario

--- resources/views/CRUD.blade.php
+++ resources/views/CRUD.blade.php
@@ -6,6 +6,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/app.css">
 
+    <style>
+        /* Opcional: Agrega estilos para el formulario */
+        
+    </style>
 
     <title>CRUD </title>
 </head>
@@ -41,10 +45,39 @@
        
     </section>
     <section>
-        <form action="">
-            <label for="">Ingresa nuevo usuario..!</label>
-            <input  type="button" value="submit">
+        <form id="mainForm">
+            <label for="nuevoUsuario">Ingresa nuevo usuario..!</label>
+            <input type="button" value="Abrir Formulario" onclick="mostrarFormulario()">
         </form>
+
+        <input type="button" value="ocultar furmulario" onclick="ocultarFormulario()">
+        
+        <form id="nuevoUsuarioForm">
+            <label for="nombre">Nombre:</label>
+            <input type="text" id="nombre" name="nombre" required>
+            <br>
+            <label for="apellido">Apellido:</label>
+            <input type="text" id="apellido" name="apellido" required>
+            <br>
+            <label for="edad">Edad:</label>
+            <input type="number" id="edad" name="edad" required>
+            <br>
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>
+            <br>
+            <input type="submit" value="Enviar">
+        </form>
+    
+        <script>
+            function mostrarFormulario() {
+                var formulario = document.getElementById("nuevoUsuarioForm");
+                formulario.style.display = "block";
+            }
+            function ocultarFormulario() {
+            var formulario = document.getElementById("nuevoUsuarioForm");
+            formulario.style.display = "none";
+        }
+        </script>
     </section>
 </body>
 </html>
